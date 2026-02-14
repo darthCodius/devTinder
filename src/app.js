edded1327709128837f6c244f2b6519f35d49f9c
Abhinav Yadav <abhiyadavpky@gmail.com>
2026-02-15 00:36:10 +0530

    Initial Project Initialized

--- src/app.js
+++ src/app.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3030;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/user", (req, res) => {
+  const dummyUser = {
+    name: "Abhinav Yadav",
+    email: "test@gmail.com",
+  };
+  res.send(dummyUser);
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save user data");
+  res.send({ success: true, message: "data saved!" });
+});
+
+app.delete("/user", (req, res) => {
+  console.log("Delete user data");
+  res.send({
+    success: true,
+    message: "deleted user data",
+  });
 });
 
 app.listen(port, () => {
