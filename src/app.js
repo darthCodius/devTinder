2a6851cb2c7989fba6121c35c9814dd030e46042
Abhinav Yadav <abhiyadavpky@gmail.com>
2026-02-24 23:18:34 +0530

    apis configured

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const { connectDb } = require("./config/database");
+const mongoose = require("mongoose");
 const User = require("./models/user");
 
 const port = 3030;
 
 app.use(express.json());
 
+// Create a user
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the User model
   const user = new User(req.body);
@@ -20,6 +22,106 @@ app.post("/signup", async (req, res) => {
     res.status(400).send({
       status: 400,
       message: `Internal Server Error, ${error}`,
+    });
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId }).exec();
+    if (!user) {
+      res.status(404).send({
+        status: 404,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).send(user);
+    }
+  } catch (error) {
+    res.status(400).send({
+      status: 400,
+      message: `Something Went Wrong`,
+    });
+  }
+});
+
+// Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({}).exec();
+    res.send(users);
+  } catch (error) {
+    res.status(400).send({
+      status: 400,
+      message: `Something Went Wrong`,
+    });
+  }
+});
+
+// Delete a user
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Validate if id is a valid MongoDB ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({
+        message: "Invalid user ID Format",
+      });
+    }
+
+    // Delete the user
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    // Check if user exists
+    if (!deletedUser) {
+      return res.status(404).send({
+        message: "User not found!",
+      });
+    }
+
+    res.status(200).send({
+      message: "User delete!",
+      data: deletedUser,
+    });
+  } catch (error) {
+    res.status(400).send({
+      status: 400,
+      message: `Something Went Wrong`,
+    });
+  }
+});
+
+// Update data of a user
+
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Validate the ObjectId in payload
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({
+        message: "Invalid user ID Format",
+      });
+    }
+
+    // Extract the data to be updated
+    const newUserData = req.body;
+
+    // Update the data, returns the updated document
+    const updatedData = await User.findByIdAndUpdate(id, newUserData, {
+      new: true,
+      upsert: false,
+    });
+    res.status(200).send({
+      message: "User Updated Successfully",
+      updatedData,
+    });
+  } catch (error) {
+    res.status(400).send({
+      status: 400,
+      message: `Something Went Wrong`,
     });
   }
 });
--- src/models/user.js
+++ src/models/user.js
@@ -5,18 +5,22 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
+    required: true,
   },
   gender: {
     type: String,
